0c7aa93ec064eb8b8280846fd6203524764c38fc
VanHilstJohann <84753317+VanHilstJohann@users.noreply.github.com>
2022-04-14 15:55:18 -0700

    Added invalid date checking & input choices

--- nextdateproblem.cpp
+++ nextdateproblem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 using namespace std;
 
@@ -9,20 +10,22 @@ bool leapyear(int year) {
         return false;
 }
 
-int main() {
-    int day, month, year = 0;
+void testdate(int day, int month, int year) {
 
-	//cout << "Enter today's date in the format:DD MM YYYY\n";
-    //cin >> day >> month >> year;
-
-    ifstream fin("dates.txt");
-
-    while (fin >> day >> month >> year) {
-
+    if (day < 0 || day > 31 || month < 1 || month > 12 || year < 1900 || year > 2025)
+        cout << day << " " << month << " " << year << " " << "is an invalid date." << endl;
+    else if (month == 2 && day > 28 && !leapyear(year))
+        cout << day << " " << month << " " << year << " " << "is an invalid date." << endl;
+    else if (month == 2 && day > 29 && leapyear(year))
+        cout << day << " " << month << " " << year << " " << "is an invalid date." << endl;
+    else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+        cout << day << " " << month << " " << year << " " << "is an invalid date." << endl;
+    else {
+        cout << "The date after " << day << " " << month << " " << year << " is:" << endl;
         if (day > 0 && day < 28)	//checking for day from 0 to 27
-            day += 1;
+                day += 1;
 
-        if (day == 28) {
+        else if (day == 28) {
             if (month == 2) {	//checking for February
                 if (leapyear(year)) {  //leap year check in case of February
                     day = 29;
@@ -33,14 +36,14 @@ int main() {
             } else	//when it's not February
                 day += 1;
         }
-        if ((day == 29) && (!leapyear(year))) {	//last day check for February
+        else if ((day == 29) && (!leapyear(year))) {	//last day check for February
             if (month == 2) {
                 day = 1;
                 month = 3;
             } else
                 day += 1;
         }
-        if (day == 30) {	//last day check for April, June, September, or November
+        else if (day == 30) {	//last day check for April, June, September, or November
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
                 day += 1;
             else {
@@ -48,7 +51,7 @@ int main() {
                 month += 1;
             }
         }
-        if (day == 31) {	//last day of the month
+         else if (day == 31) {	//last day of the month
             day = 1;
             if (month == 12) {	//checking for last day of the year
                 year += 1;
@@ -58,7 +61,7 @@ int main() {
                 month += 1;
         }
 
-        cout << "Tomorrow's date:\n";
+
         if (day < 10) {	//checking if day needs to be preceded by 0
             cout << "0" << day << " ";
         }
@@ -71,6 +74,46 @@ int main() {
             cout << month << " ";
 
         cout << year << endl;
+    }
+}
+
+ifstream getFile() {
+    string fileName;
+    cout << "Enter the filename: " << endl;
+    cin >> fileName;
+    ifstream file(fileName);
+    if (file.fail()) {
+        cout << "File " << fileName << " not found." << endl;
+        file.close();
+    }
+    else
+        return file;
+}
+
+int main() {
+    int day, month, year = 0;
+    string entry;
+    bool status = true;
+
+    while (status) {
+        cout << "Enter 't' to type in a test case, or 'f' to read from a file, or 'q' to quit: " << endl;
+        cin >> entry;
+        if (entry == "t") {
+            cout << "Enter the test date in the format: DD MM YYYY" << endl;
+            cin >> day >> month >> year;
+            cout << endl;
+            testdate(day, month, year);
+        }
+        else if (entry == "f") {
+            ifstream fin(getFile());
+            while (fin >> day >> month >> year) {
+                testdate(day, month, year);
+            }
+        }
+        else if (entry == "q") {
+            cout << "Goodbye." << endl;
+            status = false;
+        }
     }
 	return 0;
 }
